6a29c33bc88ff95919384f2dd6a67f5e8d25ada5
huouvcti <huouvcti@gmail.com>
2023-04-28 10:43:03 +0900

    /gps parameter 추가

--- controller/gpsCtrl.js
+++ controller/gpsCtrl.js
@@ -12,15 +12,22 @@ const period = async (req, res) => {
 
 const gps = async (req, res) => {
     const parameters = {
+        id: req.body.mdn || null,
         lat: req.body.latitude || null,
         long: req.body.longitude || null,
         rsrp: req.body.rsrp || null
     }
 
     console.log('gps: ', parameters);
+
+    if(parameters.id == null){
+        res.send(`Fail: mdn is undefined`)
+    } else {
+        res.send(`OK`)
+    }
     
     // res.send({"response": parameters})
-    res.send(`OK`)
+    
 }
 
 
