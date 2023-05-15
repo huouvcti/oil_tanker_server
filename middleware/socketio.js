4cc7a1c35458cbdf01a0f32de4ddcd26a3cdd1cf
huouvcti <huouvcti@gmail.com>
2023-05-15 12:01:58 +0900

    log before

--- middleware/socketio.js
+++ middleware/socketio.js
@@ -1,6 +1,9 @@
 const SocketIO = require('socket.io');
 
 
+const gpsDAO = require('../model/gpsDAO');
+
+
 const socketio = (server) => {
     const io = SocketIO(server, { path: '/socket.io' });
 
@@ -21,6 +24,11 @@ const socketio = (server) => {
             room = parseInt(data.room);
 
             socket.join(room)
+
+
+            const gps_test_before = await gpsDAO.gps.test_before();
+
+            await socket.to(room).emit('gps_client_test_before', gps_test_before);
         })
         
 
--- model/gpsDAO.js
+++ model/gpsDAO.js
@@ -2,6 +2,20 @@ const {db} = require('../config/dbconn');
 
 const gps = {}
 
+
+gps.test_before = (parameters) =>{
+    return new Promise((resolve, reject) =>{
+        db.query(`SELECT * FORM gps LIMIT 10;`, [], (err, db_data) => {
+            if(err) {
+                reject(err);
+            } else {
+                resolve(db_data);
+            }
+        })
+    })
+}
+
+
 gps.insert = (parameters) =>{
     return new Promise((resolve, reject) =>{
         db.query(`INSERT INTO gps(router_id, latitude, longitude, rsrp) VALUES(?, ?, ?, ?);`, [parameters.router_id, parameters.latitude, parameters.longitude, parameters.rsrp], (err, db_data) => {
